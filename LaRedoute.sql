96bbd5798aac0db2aca0cba9b354573d19e43c4a
Soulaimana Nadji <nadji.soulaimana@gmail.com>
2019-09-03 22:13:58 +0200

    import bdd

---
+++ LaRedoute.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: LaRedoute
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+# Database LaRedoute
+# ------------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS `LaRedoute` DEFAULT CHARACTER SET = `utf8`;
+USE `LaRedoute`;
+
+--
+-- Table structure for table `chaussures`
+--
+
+DROP TABLE IF EXISTS `chaussures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chaussures` (
+  `couleurs` varchar(255) DEFAULT NULL,
+  `prix` varchar(255) NOT NULL,
+  `marques_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `modele` varchar(255) NOT NULL,
+  `pointure` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pk_marques` (`marques_id`),
+  CONSTRAINT `pk_marques` FOREIGN KEY (`marques_id`) REFERENCES `marques` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chaussures`
+--
+
+LOCK TABLES `chaussures` WRITE;
+/*!40000 ALTER TABLE `chaussures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chaussures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commandes`
+--
+
+DROP TABLE IF EXISTS `commandes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commandes` (
+  `adresse_livraison` varchar(255) NOT NULL,
+  `clients_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `pk_clients` (`clients_id`),
+  CONSTRAINT `pk_clients` FOREIGN KEY (`clients_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commandes`
+--
+
+LOCK TABLES `commandes` WRITE;
+/*!40000 ALTER TABLE `commandes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commandes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commandes_chaussures`
+--
+
+DROP TABLE IF EXISTS `commandes_chaussures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commandes_chaussures` (
+  `id_commandes` int(11) NOT NULL,
+  `id_chaussures` int(11) NOT NULL,
+  PRIMARY KEY (`id_commandes`,`id_chaussures`),
+  KEY `pk_chaussures` (`id_chaussures`),
+  CONSTRAINT `pk_chaussures` FOREIGN KEY (`id_chaussures`) REFERENCES `chaussures` (`id`),
+  CONSTRAINT `pk_commandes` FOREIGN KEY (`id_commandes`) REFERENCES `commandes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commandes_chaussures`
+--
+
+LOCK TABLES `commandes_chaussures` WRITE;
+/*!40000 ALTER TABLE `commandes_chaussures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commandes_chaussures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marques`
+--
+
+DROP TABLE IF EXISTS `marques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marques` (
+  `nom` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marques`
+--
+
+LOCK TABLES `marques` WRITE;
+/*!40000 ALTER TABLE `marques` DISABLE KEYS */;
+/*!40000 ALTER TABLE `marques` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-02 20:42:15
